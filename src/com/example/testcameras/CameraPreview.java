8221205605a8beef9157e8d1da72886e40194d49
Dmitry Kuzminov <dima@galilsoft.com>
2013-06-24 18:23:22 +0300

    Add facilities for multi-camera

--- src/com/example/testcameras/CameraPreview.java
+++ src/com/example/testcameras/CameraPreview.java
@@ -1,110 +1,117 @@
 package com.example.testcameras;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import android.content.Context;
-import android.view.SurfaceView;
-import android.view.SurfaceHolder;
-import android.widget.Toast;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.Size;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.os.Environment;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-    private SurfaceHolder mHolder;
-    private Camera mCamera;
-    private int mId;
-    private PreviewCallback mPreviewCallback = new PreviewCallback() {
-        public void onPreviewFrame(byte[] data, Camera arg1) {     
-        	Log.d(MainActivity.TAG, "onPreviewFrame");
-            //for( int i = 0 ; i < data.length ; i ++ ){
-            //    data[ i] = 0; // or some sirius filter
-            //}        
-        	try {
-        		Camera.Parameters parameters = mCamera.getParameters();
-        		Size size = parameters.getPreviewSize();
-        		YuvImage image = new YuvImage(data, parameters.getPreviewFormat(), size.width, size.height, null);
-        		File file = new File(Environment.getExternalStorageDirectory().getPath() + "/out.jpg");
-        		FileOutputStream filecon = new FileOutputStream(file);
-        		image.compressToJpeg(new Rect(0, 0, image.getWidth(), image.getHeight()), 90, filecon);
-        	} catch (FileNotFoundException e) {
-        		Log.d(MainActivity.TAG, "Error in onPreviewFrame id " + mId + ", " + e.getMessage());
-        	}
-            CameraPreview.this.invalidate();
-            
-        }
-    };
+public class CameraPreview extends SurfaceView implements
+		SurfaceHolder.Callback {
+	private SurfaceHolder mHolder;
+	private Camera mCamera;
+	private int mId;
+	private PreviewCallback mPreviewCallback = new PreviewCallback() {
+		public void onPreviewFrame(byte[] data, Camera arg1) {
+			Log.d(MainActivity.TAG, "onPreviewFrame");
+			// for( int i = 0 ; i < data.length ; i ++ ){
+			// data[ i] = 0; // or some sirius filter
+			// }
+			try {
+				Camera.Parameters parameters = mCamera.getParameters();
+				Size size = parameters.getPreviewSize();
+				YuvImage image = new YuvImage(data,
+						parameters.getPreviewFormat(), size.width, size.height,
+						null);
+				File file = new File(Environment.getExternalStorageDirectory()
+						.getPath() + "/out.jpg");
+				FileOutputStream filecon = new FileOutputStream(file);
+				image.compressToJpeg(
+						new Rect(0, 0, image.getWidth(), image.getHeight()),
+						90, filecon);
+			} catch (FileNotFoundException e) {
+				Log.d(MainActivity.TAG, "Error in onPreviewFrame id " + mId
+						+ ", " + e.getMessage());
+			}
+			CameraPreview.this.invalidate();
 
-    public CameraPreview(Context context, Camera camera, int id) {
-        super(context);
-        mCamera = camera;
-        mId = id;
+		}
+	};
 
-        // Install a SurfaceHolder.Callback so we get notified when the
-        // underlying surface is created and destroyed.
-        mHolder = getHolder();
-        mHolder.addCallback(this);
-        // deprecated setting, but required on Android versions prior to 3.0
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
+	public CameraPreview(Context context, Camera camera, int id) {
+		super(context);
+		mCamera = camera;
+		mId = id;
 
-    public void surfaceCreated(SurfaceHolder holder) {
-    	Log.i(MainActivity.TAG, "surfaceCreated id " + mId);
-        // The Surface has been created, now tell the camera where to draw the preview.
-        try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.setPreviewCallback(mPreviewCallback);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            Log.d(MainActivity.TAG, "Error setting camera preview id " + mId + ", " + e.getMessage());
-        }
-    }
+		// Install a SurfaceHolder.Callback so we get notified when the
+		// underlying surface is created and destroyed.
+		mHolder = getHolder();
+		mHolder.addCallback(this);
+		// deprecated setting, but required on Android versions prior to 3.0
+		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-    	Log.i(MainActivity.TAG, "surfaceDestroyed id " + mId);
-        // empty. Take care of releasing the Camera preview in your activity.
-    	if (mCamera != null) {
-            mCamera.stopPreview();
-            mCamera.release();            
-            mCamera = null;
-    	}
-    }
+	public void surfaceCreated(SurfaceHolder holder) {
+		Log.i(MainActivity.TAG, "surfaceCreated id " + mId);
+		// The Surface has been created, now tell the camera where to draw the
+		// preview.
+		try {
+			mCamera.setPreviewDisplay(holder);
+			mCamera.setPreviewCallback(mPreviewCallback);
+			mCamera.startPreview();
+		} catch (IOException e) {
+			Log.d(MainActivity.TAG, "Error setting camera preview id " + mId
+					+ ", " + e.getMessage());
+		}
+	}
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-    	Log.i(MainActivity.TAG, "surfaceChanged id " + mId);
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		Log.i(MainActivity.TAG, "surfaceDestroyed id " + mId);
+		// empty. Take care of releasing the Camera preview in your activity.
+		if (mCamera != null) {
+			mCamera.stopPreview();
+			mCamera.release();
+			mCamera = null;
+		}
+	}
 
-        if (mHolder.getSurface() == null){
-          // preview surface does not exist
-          return;
-        }
+	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+		Log.i(MainActivity.TAG, "surfaceChanged id " + mId);
+		// If your preview can change or rotate, take care of those events here.
+		// Make sure to stop the preview before resizing or reformatting it.
 
-        // stop preview before making changes
-        try {
-            mCamera.stopPreview();
-        } catch (Exception e){
-          // ignore: tried to stop a non-existent preview
-        }
+		if (mHolder.getSurface() == null) {
+			// preview surface does not exist
+			return;
+		}
 
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
+		// stop preview before making changes
+		try {
+			mCamera.stopPreview();
+		} catch (Exception e) {
+			// ignore: tried to stop a non-existent preview
+		}
 
-        // start preview with new settings
-        try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.setPreviewCallback(mPreviewCallback);
-            mCamera.startPreview();
-        } catch (Exception e){
-            Log.d(MainActivity.TAG, "Error starting camera preview id " + mId + ", " + e.getMessage());
-        }
-    }
+		// set preview size and make any resize, rotate or
+		// reformatting changes here
+
+		// start preview with new settings
+		try {
+			mCamera.setPreviewDisplay(mHolder);
+			mCamera.setPreviewCallback(mPreviewCallback);
+			mCamera.startPreview();
+		} catch (Exception e) {
+			Log.d(MainActivity.TAG, "Error starting camera preview id " + mId
+					+ ", " + e.getMessage());
+		}
+	}
 }
